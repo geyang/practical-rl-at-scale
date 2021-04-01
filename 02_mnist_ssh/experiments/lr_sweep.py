e6f89584b896bb92ee973fdc34a04273f99d8f66
Ge Yang <yangge1987@gmail.com>
2021-04-01 14:10:57 -0400

    add all wip

--- 02_mnist_ssh/experiments/lr_sweep.py
+++ 02_mnist_ssh/experiments/lr_sweep.py
@@ -6,6 +6,13 @@ def launch(lr, model_name="LeNet"):
 
 if __name__ == "__main__":
     import jaynes
+    from params_proto.neo_hyper import Sweep
+
+    from mnist import Args, run
+
+    with Sweep(Args) as sweep, sweep.product:
+        Args.lr = [1e-2, 3e-2]
+        Args.seed = [100, 200, 300]
 
     jaynes.config()
     jaynes.run(launch, lr=1e-3)
